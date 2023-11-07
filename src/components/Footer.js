1901558c554cea0e41924f914ac132ebe674217f
Linh Lien <lienx149@gmail.com>
2023-11-06 21:51:51 -0600

    updating link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
           
           <a href="https://www.linkedin.com/in/linh-lien-4b4a0a51"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
           <a href="https://github.com/liex149"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-          <a href="https://www.instagram.com/linhlien23/"><img src=" https://icons8.com/icon/32323/instagram" alt="Instagram" className="icon"/></a>
+          {/* <a href="https://www.instagram.com/linhlien23/"><img src=" https://icons8.com/icon/32323/instagram" alt="Instagram" className="icon"/></a> */}
     
         </p></body>
       </div>
